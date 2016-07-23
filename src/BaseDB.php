bb5ffaabf5b57f50bb6f90f0544aec711bda9bff
maigoxin <maigoxin@gmail.com>
2016-07-23 13:43:21 +0800

    更新BaseDB

--- src/BaseDB.php
+++ src/BaseDB.php
@@ -22,12 +22,7 @@ class BaseDB
 
     final public function __get($name)
     {
-        return $this->_medoo->get($name);
-    }
-    
-    final public function __isset($name)
-    {
-        return $this->_medoo->has($name);
+        return $this->_medoo->$name;
     }
 
     final public function __call($func, $args)
@@ -37,10 +32,10 @@ class BaseDB
             $ret = call_user_func_array(array($this->_medoo, $func), $args);
             $time = $r->spent();
             $logs = $this->_medoo->log();
-            Log::Info('cache', "#method:$func#sql:".end($logs)."#ret:".json_encode($ret)."#runtime:$time");
+            Log::Info('db', "#method:$func#sql:".end($logs)."#ret:".json_encode($ret)."#runtime:$time");
             return $ret;
         }else {
-            Log::Error('cache', "#method$func#args".json_encode($args)."#message:no funcs or client is null");
+            Log::Error('db', "#method$func#args".json_encode($args)."#message:no funcs or client is null");
             return null;
         }
     }
