f3f13d8884efa88a162505653818a9f42e090d36
maigoxin <maigoxin@gmail.com>
2016-11-03 20:38:57 +0800

    增加ks3的支持

---
+++ src/Ks3.php
@@ -0,0 +1,75 @@
+<?php
+namespace EasyLib;
+class Ks3 extends Singleton 
+{
+    private $client = null;
+
+    public function __construct($ak, $sk, $endPoint)
+    {
+        //是否使用VHOST
+        define("KS3_API_VHOST", false);
+        //是否开启日志(写入日志文件)
+        define("KS3_API_LOG", false);
+        //是否显示日志(直接输出日志)
+        define("KS3_API_DISPLAY_LOG", false);
+        //是否使用HTTPS
+        define("KS3_API_USE_HTTPS", false);
+        //是否开启curl debug模式
+        define("KS3_API_DEBUG_MODE", false);
+        require_once(dirname(__FILE__).'/ks3-php-sdk/Ks3Client.class.php');
+
+        $this->client = new \Ks3Client($ak, $sk, $endPoint);
+    }
+
+    public function uploadFile($bucket, $path, $filePath, $acl = 'public-read')
+    {
+        if (!is_file($filePath)) {
+            return false;
+        }
+
+        $args = [
+            'Bucket' => $bucket,
+            'Key' => $path,
+            'Content' => [
+                'content' => $filePath,
+                'seek_position' => 0
+            ],
+            'public-read' => $acl,
+        ];
+
+        $res = $this->client->putObjectByFile($args);
+        return isset($res['ETag']) ? $res['ETag'] : false;
+    }
+
+    public function uploadDir($bucket, $path, $dirPath, $acl = 'public-read')
+    {
+        if (!is_dir($dirPath)) {
+            return false;
+        }
+
+        $box = [];
+        $this->goDir($dirPath, $box);
+        $prefixLen = strlen($dirPath);
+        foreach ($box as $file) {
+            $this->uploadFile($bucket, $path . substr($file, $prefixLen), $file, $acl);
+        }
+        return true;
+    }
+
+    public function goDir($dirPath, array &$box)
+    {
+        if (is_dir($dirPath)) {
+            $dh = opendir($dirPath);
+            while (($file = readdir($dh)) != false) {
+                if (is_dir($dirPath . '/' . $file) && $file != "." && $file != "..") {
+                    $this->goDir($dirPath . '/' . $file, $box);
+                }else {
+                    if ($file != "." && $file != "..") {
+                        $box[] = $dirPath . '/' . $file;
+                    }
+                }
+            }
+            closedir($dh);
+        }
+    }
+}
