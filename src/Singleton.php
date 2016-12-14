7e4ca485e63b5bd0b04ff29c95cd00a65815409a
maigoxin <maigoxin@gmail.com>
2016-12-14 19:07:27 +0800

    add the php 5.6version

--- src/Singleton.php
+++ src/Singleton.php
@@ -20,8 +20,12 @@ class Singleton
         $extend = implode('^_^', func_get_args());
         if (!isset(self::$instances[$cls.$extend]))
         {
-            $rcStatic=new \ReflectionClass($cls);
-            self::$instances[$cls.$extend] = $rcStatic->newInstanceArgs(func_get_args());
+            if (version_compare(PHP_VERSION, '5.6.0', '>=')) {
+                self::$instances[$cls.$extend] = new static(...func_get_args());
+            }else {
+                $rcStatic=new \ReflectionClass($cls);
+                self::$instances[$cls.$extend] = $rcStatic->newInstanceArgs(func_get_args());
+            }
         }
         return self::$instances[$cls.$extend];
     }
