9ab9c781d69841db6be34292d15d341d80e1a6a8
maigoxin <maigoxin@gmail.com>
2017-02-28 10:20:13 +0800

    增加utils基本方法

--- src/Utils.php
+++ src/Utils.php
@@ -112,4 +112,69 @@ class Utils
         }
         return $result;
     }
+
+    public static function arrayColumn(array $array, $column)
+    {
+        $result = [];
+        foreach ($array as $item) {
+            if (isset($item[$column])) {
+                $result[] = $item[$column];
+            }
+        }
+        return $result;
+    }
+
+    public static function obj2Array($obj)
+    {
+        if (is_object($obj) == false) {
+            return $obj;
+        }
+
+        $_array = get_object_vars($obj);
+
+        $array = [];
+        foreach ($_array as $key => $value) {
+            $value = (is_object($value)) ? self::obj2Array($value) : $value;
+            $array[$key] = $value;
+        }
+         
+        return $array;
+    }
+
+    public static function encrypt($string, $operation, $key='')
+    { 
+        $key=md5($key); 
+        $key_length=strlen($key); 
+          $string=$operation=='D'?base64_decode($string):substr(md5($string.$key),0,8).$string; 
+        $string_length=strlen($string); 
+        $rndkey=$box=array(); 
+        $result=''; 
+        for($i=0;$i<=255;$i++){ 
+               $rndkey[$i]=ord($key[$i%$key_length]); 
+            $box[$i]=$i; 
+        } 
+        for($j=$i=0;$i<256;$i++){ 
+            $j=($j+$box[$i]+$rndkey[$i])%256; 
+            $tmp=$box[$i]; 
+            $box[$i]=$box[$j]; 
+            $box[$j]=$tmp; 
+        } 
+        for($a=$j=$i=0;$i<$string_length;$i++){ 
+            $a=($a+1)%256; 
+            $j=($j+$box[$a])%256; 
+            $tmp=$box[$a]; 
+            $box[$a]=$box[$j]; 
+            $box[$j]=$tmp; 
+            $result.=chr(ord($string[$i])^($box[($box[$a]+$box[$j])%256])); 
+        } 
+        if($operation=='D'){ 
+            if(substr($result,0,8)==substr(md5(substr($result,8).$key),0,8)){ 
+                return substr($result,8); 
+            }else{ 
+                return''; 
+            } 
+        }else{ 
+            return str_replace('=','',base64_encode($result)); 
+        } 
+    }
 }
