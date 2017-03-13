c63b4b8ad88d0c803e6a502cdfd87170af2d27b9
maigoxin <maigoxin@gmail.com>
2017-03-13 10:46:43 +0800

    lazy cross middleware

--- src/Middleware/Cross.php
+++ src/Middleware/Cross.php
@@ -10,7 +10,8 @@ class Cross extends BaseMiddleware
         $r = $next($request, $response);
         return $r->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Method', 'GET,PUT,POST')
-            ->withHeader('Access-Control-Allow-Header', 'X-Requested-With')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->withHeader('Access-Control-Allow-Credentials', true);
     }
 }
